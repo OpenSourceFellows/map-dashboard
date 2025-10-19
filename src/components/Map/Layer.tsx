fb7363c57a6b463abb861d44c0f9a0358bf75d2a
barronbytes <jonathan.m.barron@gmail.com>
2025-10-18 20:26:29 -0400

    Comments added to Layer.tsx

--- src/components/Map/Layer.tsx
+++ src/components/Map/Layer.tsx
@@ -1,17 +1,24 @@
-// Given a Feature, it will return a Leaflet Component based on geometry type
-import React from 'react'
+import { type JSX } from 'react'
 import { GeoJSON } from 'react-leaflet'
 import type { FeatureCollection } from '@/types/geometry'
-import type { Feature } from 'geojson'
+// import type { Feature } from 'geojson'
 
 interface LayerProps {
   featureCollection: FeatureCollection
 }
 
-export const Layer: React.FC<LayerProps> = ({ featureCollection }) => {
+/**
+ * Returns a Leaflet <GeoJSON> component for each Feature in a FeatureCollection,
+ * based on appropriate layer geometry type.
+ *
+ * @param {LayerProps} props - The component props.
+ * @param {FeatureCollection} props.featureCollection - A GeoJSON FeatureCollection containing one or more features.
+ * @returns {JSX.Element[]} An array of <GeoJSON> components, one per Feature in the collection.
+ */
+export function Layer({ featureCollection }: LayerProps): JSX.Element[] {
   return (
     featureCollection.features.map((feature, idx) => (
-      <GeoJSON key={idx}  data={feature}/>
+      <GeoJSON key={idx} data={feature} />
     ))
   )
 }
